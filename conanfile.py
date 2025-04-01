95284a765e4dc2ebcbdc1d77f7c3a311802bbe04
Maxx Slash“ <wanwei@sinolink-technologies.com>
2025-04-01 14:38:40 +0800

    fix

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile, tools
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
-from conan.tools.files import get, copy, rmdir
+from conan.tools.files import get, copy, rmdir, collect_libs
 from conan.tools.layout import cmake_layout
 import os
 
@@ -46,6 +46,13 @@ class CUnitConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
         tc.variables["BUILD_TESTING"] = self.options.build_testing
+
+        if self.settings.compiler == "Visual Studio":
+            if self.settings.build_type == "Release" and "MD" == str(self.settings.compiler.runtime):
+                print("compiler.runtime is MD")
+                tc.preprocessor_definitions.debug["CMAKE_MSVC_RUNTIME_LIBRARY"] = "MultiThreadedDLL"
+
+
         tc.generate()
 
         deps = CMakeDeps(self)
@@ -56,10 +63,23 @@ class CUnitConan(ConanFile):
         cmake.configure()
         cmake.build()
 
+
+
+
     def package(self):
         cmake = CMake(self)
         cmake.install()
-        
+
+        # move file from root to include/CUnit/Headers and remove *.h under include
+        copy(self, "*.h", os.path.join(self.source_folder, 'CUnit', 'Headers'), os.path.join(self.package_folder, "include/CUnit"), keep_path=True)
+        # remove *.h under include
+        for header in os.listdir(os.path.join(self.package_folder, "include")):
+            if header.endswith(".h"):
+                os.remove(os.path.join(self.package_folder, "include", header))
+
+        copy(self, "*.lib", self.build_folder, os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.dll", self.build_folder, os.path.join(self.package_folder, "bin"), keep_path=False)
+
         # Copy license file
         copy(self, "COPYING", src=self.export_sources_folder, dst=os.path.join(self.package_folder, "licenses"))
         
@@ -67,25 +87,19 @@ class CUnitConan(ConanFile):
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "share"))
 
-    def package_info(self):
-        self.cpp_info.set_property("cmake_file_name", "CUnit")
-        self.cpp_info.set_property("cmake_target_name", "CUnit::cunit")
-        self.cpp_info.set_property("pkg_config_name", "cunit")
+       
 
-        # Libraries
-        self.cpp_info.libs = ["cunit"]
+
+    def package_info(self):
+        self.cpp_info.libs = collect_libs(self)
+        self.output.info("package_info() " + ' '.join(self.cpp_info.libs))
+    
+
         if self.settings.os == "Windows" and self.options.shared:
             self.cpp_info.defines = ["CUNIT_DLL"]
-
-        # Include directories
-        self.cpp_info.includedirs = ["include"]
 
         # Build type specific settings
         if self.settings.build_type == "Debug":
             self.cpp_info.defines.append("CU_DLL=1")
             if self.options.shared:
                 self.cpp_info.defines.append("CU_BUILD_DLL")
-
-        # Set package info
-        self.cpp_info.names["cmake_find_package"] = "CUnit"
-        self.cpp_info.names["cmake_find_package_multi"] = "CUnit" 
